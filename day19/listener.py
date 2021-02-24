fd766a28f79174cbba831ade5e18db3f36aef31f
leigh90 <zawadikibathi@gmail.com>
2021-02-24 14:40:38 +0300

    Snake game part one --wip--

--- day19/listener.py
+++ day19/listener.py
@@ -3,7 +3,7 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 
-tim.speed(0.0010)
+tim.speed(0.10)
 # # tim.forward(100)
 # # tim.right(180)
 # # tim.forward(100)
@@ -17,43 +17,58 @@ tim.speed(0.0010)
 # tim.settiltangle(30)
 # tim.fd(50)
 tim.forward(100)
-tim.clear()
+# go up
+# tim.setheading(90)
+# tim.forward(100)
+# # turn left
+tim.setheading(180)
+tim.forward(100)
+# go down
+# tim.setheading(270)
+# tim.forward(120)
+# # go right
+# tim.setheading(0)
+# tim.forward(100)
+
+
+
+# tim.clear()
 
 
 
 
 
-def move_forward():
-    tim.forward(100)
-
-def move_backward():
-    tim.backward(100)
-
-def turn
-def counter_clockwise():
-    tim.right(10)
-    # tim.forward(100)
-    pass
-
-def anticlockwise():
-    tim.left(10)
-    pass
-
-def clear_drawing():
-    tim.clear()
-    tim.penup()
-    tim.home()
-
-
-screen.listen()
-# onkey is a higherorder function that takes in a function and a eventlistenter and calls the function when the event is triggered.
-# try to use positional arguments for easy readability and understandability
-screen.onkey(key='space', fun=move_forward)
-screen.onkey(key='w', fun=move_forward)
-screen.onkey(key='s', fun=move_backward)
-screen.onkey(key='a', fun=counter_clockwise)
-screen.onkey(key='d', fun=anticlockwise)
-screen.onkey(key='c', fun=clear_drawing)
+# def move_forward():
+#     tim.forward(100)
+#
+# def move_backward():
+#     tim.backward(100)
+#
+# def turn
+# def counter_clockwise():
+#     tim.right(10)
+#     # tim.forward(100)
+#     pass
+#
+# def anticlockwise():
+#     tim.left(10)
+#     pass
+#
+# def clear_drawing():
+#     tim.clear()
+#     tim.penup()
+#     tim.home()
+#
+#
+# screen.listen()
+# # onkey is a higherorder function that takes in a function and a eventlistenter and calls the function when the event is triggered.
+# # try to use positional arguments for easy readability and understandability
+# screen.onkey(key='space', fun=move_forward)
+# screen.onkey(key='w', fun=move_forward)
+# screen.onkey(key='s', fun=move_backward)
+# screen.onkey(key='a', fun=counter_clockwise)
+# screen.onkey(key='d', fun=anticlockwise)
+# screen.onkey(key='c', fun=clear_drawing)
 
 
 screen.exitonclick()
