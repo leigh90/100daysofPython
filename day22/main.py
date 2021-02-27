b0ee4481a61313816a7f3668d103e0aa908d2a53
leigh90 <zawadikibathi@gmail.com>
2021-02-27 03:20:22 +0300

    create the background screen

---
+++ day22/main.py
@@ -0,0 +1,18 @@
+from turtle import Screen
+
+
+
+
+# SETUP SCREEN
+screen = Screen()
+screen.setup(width=800, height=800)
+screen.bgcolor("black")
+screen.title('Pong')
+
+
+
+
+
+
+
+screen.exitonclick()
