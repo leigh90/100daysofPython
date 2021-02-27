7be1b425f17498052e70b10e695e0067d47629e0
leigh90 <zawadikibathi@gmail.com>
2021-02-27 04:03:42 +0300

    Add Paddle class

--- day22/main.py
+++ day22/main.py
@@ -1,5 +1,5 @@
 from turtle import Screen
-
+from paddle import Paddle
 
 
 
@@ -8,7 +8,23 @@ screen = Screen()
 screen.setup(width=800, height=800)
 screen.bgcolor("black")
 screen.title('Pong')
+screen.tracer(0)   # to remove the animation
 
+
+# SET UP OBJECTS
+paddle = Paddle()
+
+
+screen.listen()
+screen.onkey(key='m', fun=paddle.paddleup)
+screen.onkey(key="Down", fun=paddle.paddledown)
+
+
+
+game_on = True
+
+while game_on:
+    screen.update()  # this updates the screen after creating the turtle to cremove the animation
 
 
 
