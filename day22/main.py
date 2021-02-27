3017a57b57ca686f8c1c0956f2a09203d598d973
leigh90 <zawadikibathi@gmail.com>
2021-02-27 04:43:11 +0300

    Refactor paddle class

--- day22/main.py
+++ day22/main.py
@@ -1,34 +1,26 @@
 from turtle import Screen
 from paddle import Paddle
 
-
-
 # SETUP SCREEN
 screen = Screen()
 screen.setup(width=800, height=800)
 screen.bgcolor("black")
 screen.title('Pong')
-screen.tracer(0)   # to remove the animation
-
+screen.tracer(0)  # to remove the animation
 
 # SET UP OBJECTS
-paddle = Paddle()
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(key='m', fun=paddle.paddleup)
-screen.onkey(key="Down", fun=paddle.paddledown)
-
-
+screen.onkey(key='m', fun=r_paddle.paddleup)
+screen.onkey(key="Down", fun=r_paddle.paddledown)
+screen.onkey(key='w', fun=l_paddle.paddleup)
+screen.onkey(key="s", fun=l_paddle.paddledown)
 
 game_on = True
 
 while game_on:
     screen.update()  # this updates the screen after creating the turtle to cremove the animation
-
-
-
-
-
 
 screen.exitonclick()
--- day22/paddle.py
+++ day22/paddle.py
@@ -2,19 +2,19 @@ from turtle import Turtle, Screen
 
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
+        self.setposition(position)
         self.shape('square')
         self.color('white')
         self.penup()
         self.shapesize(stretch_wid=5, stretch_len=1)
-        self.goto(350, 0)
+        # self.goto(350, 0)
         # self.setposition(350, 0)
 
     def paddleup(self):
         new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
-
 
     def paddledown(self):
         new_y = self.ycor() - 20
