68f9d2ac281c895f3c0852f616857fb537685214
leigh90 <zawadi.kibathi@gmail.com>
2021-02-28 16:20:23 +0300

    Setup player and movement

--- day23/player.py
+++ day23/player.py
@@ -1,7 +1,20 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.color('black')
+        self.penup()
+        self.setposition(0, -270)
+        self.setheading(90)
+        self.move_distance = MOVE_DISTANCE
+
+    def move(self):
+        self.forward(self.move_distance)
+
 
