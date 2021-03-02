77cd5b106e663b5a1bc6b7dafd5cb4614af5de82
leigh90 <zawadi.kibathi@gmail.com>
2021-03-02 07:02:26 +0300

    Add car behavior and detect collision with turtle

--- day23/car_manager.py
+++ day23/car_manager.py
@@ -6,10 +6,11 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager():
     def __init__(self):
-        super().__init__()
+        # super().__init__()
         self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
     def create_car(self):
         """
@@ -21,13 +22,16 @@ class CarManager(Turtle):
             new_car.shapesize(stretch_len=3)
             new_car.penup()
             new_car.color(random.choice(COLORS))
-            random_y = random.randint(-280,280)
-            new_car.goto(300,random_y)
+            random_y = random.randint(-280, 280)
+            new_car.goto(300, random_y)
             self.all_cars.append(new_car)
 
     def move_cars(self):
         for car in self.all_cars:
             car.backward(STARTING_MOVE_DISTANCE)
+
+    def add_speed(self):
+        self.car_speed += MOVE_INCREMENT
 
 
 
--- day23/main.py
+++ day23/main.py
@@ -17,15 +17,25 @@ screen.listen()
 screen.onkey(key='m', fun=player.move)
 
 
-
-
-
 game_on = True
 while game_on:
     time.sleep(0.1)
     screen.update()
+
     car_manager.create_car()
     car_manager.move_cars()
+
+    # detect turtle collision
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            print('too close')
+            game_on = False
+
+    #   detect if the turtle has reached the finish line
+    if player.ycor() > 290:
+        player.player_reset()
+        car_manager.add_speed()
+
 
 
 screen.exitonclick()
