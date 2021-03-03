26ca47ba35b9cb8780fdceccb998fa5ce5279681
leigh90 <zawadi.kibathi@gmail.com>
2021-03-03 04:19:02 +0300

    Add highscore and resent current score on game over

---
+++ day24/main2.py
@@ -0,0 +1,72 @@
+from turtle import Screen
+from snake2 import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title('Snake Ninja')
+screen.tracer(0)
+
+# CREATE A SNAKE
+snake = Snake()
+# CREATE FOOD
+food = Food()
+scoreboard = Scoreboard()
+
+# CONTROL SNAKE
+screen.listen()
+screen.onkey(key="m", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+# OR
+# screen.onkey(snake.up, "Up")
+# screen.onkey(snake.down, "Down")
+# screen.onkey(snake.left, "Left")
+# screen.onkey(snake.right, "Right")
+
+game_on = True
+while game_on:
+    # delay updating the screen by 0.1 seconds
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # FOOD COLLISION
+    # distance is a turtle method that allows you to check the distance between two turtle objects in this instance the snake head and the food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend_snake()
+        scoreboard.increase_score()
+
+    # DETECT WALL COLLISION
+    if snake.head.xcor() > 280  or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.rese()
+
+#     DETECT TAIL COLLISION
+#     for segment in snake.segments:
+#         if segment == snake.head:
+#             pass
+#         elif snake.head.distance(segment) < 10:
+#             game_on = False
+#             scoreboard.game_over()
+
+    # OR
+    # everything except the head
+    for segment in snake.full_snake[1:]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.reset()
+
+
+
+
+
+
+
+
+screen.exitonclick()
---
+++ day24/snake2.py
@@ -0,0 +1,107 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.full_snake = []
+        self.create_snake()
+        self.head = self.full_snake[0]
+
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape='square')
+        new_segment.color('white')
+        new_segment.penup()
+        new_segment.goto(position)
+        self.full_snake.append(new_segment)
+
+    def extend_snake(self):
+        self.add_segment(self.full_snake[-1].position())
+
+    def move(self):
+        for snake_seg in range(len(self.full_snake)-1,0,-1):
+            new_x = self.full_snake[snake_seg-1].xcor()
+            new_y = self.full_snake[snake_seg-1].ycor()
+            self.full_snake[snake_seg].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    # go up
+    # tim.setheading(90)
+    # tim.forward(100)
+    # # turn left
+    # tim.setheading(180)
+    # tim.forward(100)
+    # go down
+    # tim.setheading(270)
+    # tim.forward(120)
+    # # go right
+    # tim.setheading(0)
+    # tim.forward(100)
+
+
+    from turtle import Screen, Turtle
+
+
+
+    import time
+    #
+    # screen = Screen()
+    # screen.setup(width=600, height=600)
+    # screen.bgcolor("black")
+    # screen.title('Snake Ninja')
+    # screen.tracer(0)
+    #
+    # # STEP 1: SNAKE BODY
+    # segments = []
+    # positions = [(0, 0), (-20, 0), (-40, 0)]
+    # for position in positions:
+    #     new_segment = Turtle(shape='square')
+    #     new_segment.color('white')
+    #     new_segment.penup()
+    #     new_segment.goto(position)
+    #     segments.append(new_segment)
+    #
+    # print(len(segments))
+    # # MOVE THE SNAKE
+    # game_on = True
+    # while game_on:
+    #     screen.update()
+    #     time.sleep(0.1)
+    #     for seg_num in range(len(segments) - 1, 0, -1):
+    #         new_x = segments[seg_num - 1].xcor()
+    #         print(f'seg_num is {seg_num}')
+    #         print(f' segments is {segments}')
+    #         print(f'segments[seg_num] is {segments[seg_num]}')
+    #         new_y = segments[seg_num - 1].ycor()
+    #         segments[seg_num].goto(new_x, new_y)
+    #     segments[0].forward(20)
+    #     segments[0].left(90)
+    #
+    # screen.exitonclick()
