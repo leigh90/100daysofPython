1a1b6a60e1e23eea6dc6af2bd4950bf60e359fdf
leigh90 <zawadi.kibathi@gmail.com>
2021-04-01 17:08:20 +0300

    Update main.py to use a list comprehension

--- day25/USstates/us-states-game-start/main.py
+++ day25/USstates/us-states-game-start/main.py
@@ -19,10 +19,12 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)} Guess the state", prompt="Add another state").title()
     if answer_state == 'Exit':
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        # OR
+        missing_states = [state for state in all_states if state not in guessed_states]
         print(missing_states)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv('states_tolearn.csv')
