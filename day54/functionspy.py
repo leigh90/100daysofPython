5f67128fd928482bf35da0875cc02a8649ab6bbd
leigh90 <zawadi.kibathi@gmail.com>
2021-04-18 17:03:39 +0300

    Python decorators.py

--- day54/functionspy.py
+++ day54/functionspy.py
@@ -20,29 +20,50 @@
 # result = calculator(4,5,multiply)
 # print(result)
 #
-# # todo nested Functions
-def outer_funcion():
-    print('Im outer')
+# # # todo nested Functions
+# def outer_funcion():
+#     print('Im outer')
+#
+#     def nestedfunction():
+#         print('Im inner')
+#
+#     nestedfunction()
+#
+# outer_funcion()
+#
+# # todo Functions can be returned from other functions
+# def outer_function():
+#     print('Im outer')
+#
+#     def nestedfunction():
+#         print('Im inner')
+#
+#     return nestedfunction
+#
+# new_var = outer_function()
+# print(type(outer_function))
+# new_var()
+# # print(type(new_var))
 
-    def nestedfunction():
-        print('Im inner')
+# TODO PYTHON DECORATORS
+# a decorator wraps another function either modifying it or adding to its functionality
+import time
 
-    nestedfunction()
-
-outer_funcion()
-
-# todo Functions can be returned from other functions
-def outer_function():
-    print('Im outer')
-
-    def nestedfunction():
-        print('Im inner')
-
-    return nestedfunction
-
-new_var = outer_function()
-print(type(outer_function))
-new_var()
-# print(type(new_var))
+def decorator_function(function):
+    def wrapper_function():
+        time.sleep(5)
+        function()
+    return wrapper_function
 
 
+@decorator_function
+def say_hello():
+    print('Ssup boo')
+
+def say_greeting():
+    print('Hello love')
+say_hello()
+
+# todo the @decorator is basically syntactic sugar for passing in the function to another function and then assigning it to a variable
+decorated_var = decorator_function(say_greeting)
+decorated_var()
