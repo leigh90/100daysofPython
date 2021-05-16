28f5f22d8045c38a692ba3402298298b57d5d642
leigh90 <zawadi.kibathi@gmail.com>
2021-05-16 06:31:09 +0300

    Error catching

---
+++ day30/main.py
@@ -0,0 +1,5 @@
+# try: Something that might cause an Exception (this may or may not work)
+# except: do this if there was an exception 
+# else: do this if there were no exceptions
+# finally: do this no matter what happens, this block is always execued
+
