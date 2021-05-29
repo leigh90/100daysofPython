fde4b92b308502e505dccbbe9b296edbb1114feb
leigh90 <zawadi.kibathi@gmail.com>
2021-05-29 16:33:21 +0300

    Agify and genderize api to add variables

--- day57/main.py
+++ day57/main.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 from flask import render_template
 import random
 from datetime import date
+import requests
 
 @app.route('/')
 def index():
@@ -17,6 +18,37 @@ def index():
      # >>>2021
     return render_template('index.html',num=random_num, year=current_year)
 
+
+
+@app.route('/guess/<guessname>')
+def guess(guessname):
+    parameters = {
+        'name': guessname
+    }
+    # agify_response = requests.get(url="https://api.agify.io",params=parameters)
+    # gender_response = requests.get(url="https://api.genderize.io",params=parameters)
+    # or 
+    agify_response = requests.get(url=f"https://api.agify.io/?name={guessname}")
+    gender_response = requests.get(url=f"https://api.genderize.io/?name={guessname}")
+
+    agify_response.raise_for_status()
+    dataone = agify_response.json()
+    datatwo = gender_response.json()
+    print(dataone)
+    agify_name = dataone["name"]
+    print(agify_name)
+    agyify_age=dataone["age"]
+    print(agyify_age)
+    print(datatwo)
+    gender = datatwo["gender"]
+    print(gender)
+    return render_template('index.html',name=agify_name,age=agyify_age, gender=gender )
+
+
+# guess('ashley')
+
+
+    
 
 
 
