6919ede022e766faec9e906fce6849419bdf18c9
leigh90 <zawadi.kibathi@gmail.com>
2021-05-29 14:40:59 +0300

    Add Namecard project - Rendering html files in flask

---
+++ day56/NameCard/main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
